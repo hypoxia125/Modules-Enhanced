00bbce0e372727a83bd29cec01b372d93072e5ca
Hypoxic <60307173+hypoxia125@users.noreply.github.com>
2023-09-21 23:27:51 -0400

    [Add Module] Vehicle Rearm
    Adds vehicle rearm module
    
    Attributes:
    
    - Repeatable - Choose if to repeat or not
    - Time Delay - Delay in time between rearms

--- addons/modules/CfgVehicles.hpp
+++ addons/modules/CfgVehicles.hpp
@@ -24,4 +24,5 @@ class CfgVehicles {
     #include "\z\meh\addons\modules\modules\moduleFuelConsumption.hpp"
     #include "\z\meh\addons\modules\modules\moduleMoveOnCombat.hpp"
     #include "\z\meh\addons\modules\modules\moduleParadropVehicle.hpp"
+    #include "\z\meh\addons\modules\modules\moduleVehicleRearm.hpp"
 };
