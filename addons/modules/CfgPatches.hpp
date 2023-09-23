18a5eda9359c99086832da3fc6932986387186ca
Hypoxic <martini_a@outlook.com>
2023-09-23 12:50:22 -0400

    update

--- addons/modules/CfgPatches.hpp
+++ addons/modules/CfgPatches.hpp
@@ -14,6 +14,7 @@ class CfgPatches {
             "MEH_ModuleFuelConsumption",
             "MEH_ModuleMoveOnCombat",
             "MEH_ModuleParadropVehicle",
+            "MEH_ModuleSpeedLimiter",
             "MEH_ModuleVehicleMineJammer",
             "MEH_ModuleVehicleRearm",
             "MEH_ModuleVehicleRefuel"
