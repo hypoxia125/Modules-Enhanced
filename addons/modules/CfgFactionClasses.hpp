b37f26428f6b77f59ac1e680c8afaaffefc343b4
Hypoxic <martini_a@outlook.com>
2024-11-12 16:38:44 -0700

    Added - Additional CfgFactionClasses

--- addons/modules/CfgFactionClasses.hpp
+++ addons/modules/CfgFactionClasses.hpp
@@ -3,4 +3,12 @@ class CfgFactionClasses {
         displayName = ECSTRING(Main,ModulesEnhanced); // Todo: Localize String
         side = 7;
     };
+    class MEH_Effects {
+        displayName = "Modules Enhanced - Effects";
+        side = 7;
+    };
+    class MEH_Logistics {
+        displayName = "Modules Enhanced - Logistics";
+        side = 7;
+    };
 };
