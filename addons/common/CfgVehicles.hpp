17a6f9b731e3800a206bb5297c10da39665a1ca5
Hypoxic <martini_a@outlook.com>
2024-12-02 22:25:44 -0700

    added inverted modules enhanced flags

--- addons/common/CfgVehicles.hpp
+++ addons/common/CfgVehicles.hpp
@@ -11,6 +11,14 @@ class CfgVehicles {
         scopeCurator = 2;
     };
 
+    class Flag_MEH_Inverted_F: Flag_MEH_F {
+        displayName = "Flag (Modules Enhanced - Inverted)";
+        editorPreview = "\z\meh\addons\common\data\textures\flags\flag_MEH_Inverted_co.paa";
+        class EventHandlers {
+            init = "(_this select 0) setFlagTexture '\z\meh\addons\common\data\textures\flags\flag_MEH_Inverted_co.paa'";
+        };
+    };
+
     class Banner_01_base_F;
     class Banner_MEH_F: Banner_01_base_F {
         author = "Hypoxic";
@@ -19,5 +27,11 @@ class CfgVehicles {
         hiddenSelectionsTextures[] = {"\z\meh\addons\common\data\textures\flags\flag_MEH_co.paa"};
         scope = 2;
         scopeCurator = 2;
+    };
+
+    class Banner_MEH_Inverted_F: Banner_MEH_F {
+        displayName = "Banner (Modules Enhanced - Inverted)";
+        editorPreview = "\z\meh\addons\common\data\textures\flags\flag_MEH_Inverted_co.paa";
+        hiddenSelectionsTextures[] = {"\z\meh\addons\common\data\textures\flags\flag_MEH_Inverted_co.paa"};
     };
 };
