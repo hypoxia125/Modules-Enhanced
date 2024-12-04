e1e715588bb9dde5a5d1b8b42c043c502117e775
Hypoxic <martini_a@outlook.com>
2024-12-04 00:26:28 -0700

    Update Cfg3DEN.hpp

--- addons/common/Cfg3DEN.hpp
+++ addons/common/Cfg3DEN.hpp
@@ -20,7 +20,40 @@ class Cfg3DEN {
                 };
             };
         };
+
+        class Slider: Title {
+            class Controls: Controls {
+                // class Title;
+                // class Edit;
+                class Value;
+            };
+        };
+        class MEH_ModuleSlider_Int: Slider {
+            attributeLoad = "\
+                _ctrlGroup = _this;\
+                [_ctrlGroup controlsgroupctrl 100,_ctrlGroup controlsgroupctrl 101,'',_value] call bis_fnc_initSliderValue;\
+            ";
+            attributeSave = "\
+                sliderPosition (_this controlsGroupCtrl 100);
+            ";
+            onLoad = "\
+                _ctrlGroup = _this select 0;\
+                [_ctrlGroup controlsgroupctrl 100,_ctrlGroup controlsgroupctrl 101,''] call bis_fnc_initSliderValue;\
+            ";
+
+            class Controls: Controls {
+                // class Title: Title {};
+                // class Edit: Edit {};
+                class Value: Value {
+                    sliderRange[] = {1, 10};
+                    sliderPosition = 5;
+                    sliderStep = 1;
+                    lineSize = 0.25;
+                };
+            };
+        };
     };
+
     class EventHandlers {
         class MEH {
             // Use CBA cache in uiNamespace due to the event handlers firing before the functions are added to missionNamespace
@@ -28,6 +61,7 @@ class Cfg3DEN {
             onMissionLoad = "call (uiNamespace getVariable [""MEH_Common_fnc_3DEN_OnMissionLoad"", {}])";
         };
     };
+
     class Mission {
         class Scenario {
             class AttributeCategories {
